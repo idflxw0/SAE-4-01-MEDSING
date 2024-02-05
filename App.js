f3a2d80878c6af2dac16840443ea640ecdcc80eb
riadfrz <riadfarouzi@gmail.com>
2024-02-05 19:37:04 +0100

    splash and homer pâge

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from "./src/pages/SplashScreen";
+import HomeScreen from "./src/pages/HomeScreen";
+
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
       <NavigationContainer>
           <Stack.Navigator initialRouteName="SplashScreen" headerMode="none">
-              <Stack.Screen name="SplashScreen" component={SplashScreen}></Stack.Screen>
+              <Stack.Screen name="SplashScreen" component={SplashScreen} />
+              <Stack.Screen name="HomeScreen" component={HomeScreen} />
           </Stack.Navigator>
       </NavigationContainer>
   );
