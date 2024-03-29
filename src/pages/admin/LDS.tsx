84cfd811090c546bac16f35efb5c7cb6c374889d
riadfrz <riadfarouzi@gmail.com>
2024-03-29 16:34:51 +0100

    ldh

--- src/pages/admin/LDS.tsx
+++ src/pages/admin/LDS.tsx
@@ -176,7 +176,7 @@ const styles = StyleSheet.create({
     },
     optionText: {
         flex: 1,
-        fontSize: 10,
+        fontSize: 7,
         fontWeight: 'bold',
         textAlign: 'center',
     },
