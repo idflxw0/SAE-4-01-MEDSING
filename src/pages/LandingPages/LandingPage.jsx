b5858d00fdabb375b0cfb237d081f106ba230c9a
Flaikos <flaikebs@gmail.com>
2024-03-31 08:38:58 +0200

    landing page

--- src/pages/LandingPages/LandingPage.jsx
+++ src/pages/LandingPages/LandingPage.jsx
@@ -1,139 +1,193 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Image, Animated } from 'react-native';
-import styled from 'styled-components/native';
+import React, { useState, useRef, useEffect } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, Animated, Easing } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import Carousel, { Pagination } from 'react-native-snap-carousel';
 
 const OnboardingScreen = ({ navigation }) => {
-    const [currentPage, setCurrentPage] = useState(0);
+    const [activeIndex, setActiveIndex] = useState(0);
+    const carouselRef = useRef(null);
+    const buttonScale = useRef(new Animated.Value(1)).current;
 
     const handleSignUpPress = () => {
+        animateButton();
         navigation.navigate('SignUp');
     };
 
     const handleLogInPress = () => {
+        animateButton();
         navigation.navigate('SignIn');
     };
 
-    const landingPages = [
-        {
-            image: require('./assets/LandingImages/MEDSING.png'),
-            text: 'Bienvenue sur Medsing !',
-        },
-        {
-            image: require('./images/page2.jpg'),
-            text: 'Signaler des médicaments',
-        },
-        {
-            image: require('./images/page3.jpg'),
-            text: 'Observer votre historique',
-        },
-        {
-            image: require('./images/page4.jpg'),
-            text: 'Scanner vos médicaments',
-        },
-    ];
-
-    const handlePageChange = (index) => {
-        setCurrentPage(index);
+    const animateButton = () => {
+        Animated.sequence([
+            Animated.timing(buttonScale, {
+                toValue: 0.9,
+                duration: 50,
+                easing: Easing.linear,
+                useNativeDriver: true,
+            }),
+            Animated.timing(buttonScale, {
+                toValue: 1,
+                duration: 50,
+                easing: Easing.linear,
+                useNativeDriver: true,
+            }),
+        ]).start();
     };
 
+    const carouselItems = [
+        { title: 'Bienvenue sur MedSing !'},
+        { title: 'Signaler des médicaments', paragraph: 'Saissisez le code CIP du médicament à signaler qui susciste des difficultés de renouvellement ' },
+        { title: 'Observer votre historique', paragraph: 'Vous pouvez suivre tous vos signalements depuis la page Historique' },
+        { title: 'Scanner vos médicaments', paragraph: 'Utilisez votre caméra pour signaler vos médicaments à l’aide du DataMatrix, plutôt que de saisir le code CIP' },
+    ];
+
+    const renderCarouselItem = ({ item, index }) => {
+        return (
+            <View style={styles.carouselItem}>
+                <Text style={styles.carouselTitle}>{item.title}</Text>
+                <Text style={styles.carouselParagraph}>{item.paragraph}</Text>
+            </View>
+        );
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (carouselRef.current) {
+                carouselRef.current.snapToNext();
+            }
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
-        <Container>
+        <View style={styles.container}>
             <LinearGradient
                 colors={['#B7FFB1', '#FFE500']}
                 style={styles.background}
             />
-            <Content>
-                <Pages>
-                    {landingPages.map((page, index) => (
-                        <Page key={index}>
-                            <PageImage source={page.image} />
-                            <PageText>{page.text}</PageText>
-                        </Page>
-                    ))}
-                </Pages>
-                <IndicatorContainer>
-                    {landingPages.map((_, index) => (
-                        <PageIndicator
-                            key={index}
-                            active={index === currentPage}
-                        />
-                    ))}
-                </IndicatorContainer>
-                <Button onPress={handleSignUpPress} blue>
-                    <ButtonText>Inscription</ButtonText>
-                </Button>
-                <Button onPress={handleLogInPress} red>
-                    <ButtonText>Connexion</ButtonText>
-                </Button>
-            </Content>
-        </Container>
+            <View style={styles.carouselContainer}>
+                <Carousel
+                    ref={carouselRef}
+                    data={carouselItems}
+                    renderItem={renderCarouselItem}
+                    sliderWidth={300}
+                    itemWidth={300}
+                    layout={'default'}
+                    loop={true}
+                    onSnapToItem={(index) => setActiveIndex(index)}
+                />
+            </View>
+            <Pagination
+                dotsLength={carouselItems.length}
+                activeDotIndex={activeIndex}
+                containerStyle={styles.paginationContainer}
+                dotStyle={styles.paginationDot}
+                inactiveDotStyle={styles.inactivePaginationDot}
+                inactiveDotOpacity={0.4}
+                inactiveDotScale={0.6}
+            />
+            <View style={styles.footer}>
+                <Animated.View style={[styles.buttonContainer, { transform: [{ scale: buttonScale }] }]}>
+                    <TouchableOpacity
+                        style={styles.signUpButton}
+                        onPress={handleSignUpPress}
+                    >
+                        <Text style={styles.signUpButtonText}>Sign Up</Text>
+                    </TouchableOpacity>
+                </Animated.View>
+                <Animated.View style={[styles.buttonContainer, { transform: [{ scale: buttonScale }] }]}>
+                    <TouchableOpacity
+                        style={styles.loginButton}
+                        onPress={handleLogInPress}
+                    >
+                        <Text style={styles.buttonText}>Login</Text>
+                    </TouchableOpacity>
+                </Animated.View>
+            </View>
+        </View>
     );
 };
 
-const Container = styled.View`
-    flex: 1;
-`;
-
-const Content = styled.View`
-    flex: 1;
-    align-items: center;
-    justify-content: center;
-    padding-horizontal: 20px;
-`;
-
-const Pages = styled.View`
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    margin-bottom: 20px;
-`;
-
-const Page = styled.View`
-    align-items: center;
-`;
-
-const PageImage = styled(Image)`
-    width: 300px;
-    height: 300px;
-    resize-mode: contain;
-    margin-bottom: 20px;
-`;
-
-const PageText = styled.Text`
-    color: #000;
-    text-align: center;
-    font-size: 18px;
-    margin-bottom: 20px;
-`;
-
-const IndicatorContainer = styled.View`
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    margin-bottom: 20px;
-`;
-
-const PageIndicator = styled.View`
-    width: 10px;
-    height: 10px;
-    border-radius: 5px;
-    background-color: ${(props) => (props.active ? '#00f' : '#000')};
-    margin-horizontal: 5px;
-`;
-
-const Button = styled.TouchableOpacity`
-    background-color: ${(props) => (props.blue ? '#3498db' : props.red ? '#e74c3c' : '#FF6347')};
-    padding-vertical: 15px;
-    padding-horizontal: 40px;
-    border-radius: 25px;
-    margin-vertical: 10px;
-`;
-
-const ButtonText = styled.Text`
-    color: #FFF;
-    font-size: 18px;
-    text-align: center;
-`;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    background: {
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+    },
+    carouselContainer: {
+        marginTop: 700,
+        marginBottom: 20,
+    },
+    carouselItem: {
+        borderRadius: 5,
+        padding: 20,
+        marginHorizontal: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    carouselTitle: {
+        fontSize: 35,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    carouselParagraph: {
+        fontSize: 16,
+        color: '#888',
+    },
+    footer: {
+        alignItems: 'center',
+        marginBottom: 400,
+    },
+    buttonContainer: {
+        marginBottom: 10,
+    },
+    signUpButton: {
+        backgroundColor: '#FFB6C1',
+        paddingVertical: 10,
+        paddingHorizontal: 30,
+        borderRadius: 30,
+    },
+    loginButton: {
+        backgroundColor: '#ADD8E6',
+        paddingVertical: 10,
+        paddingHorizontal: 30,
+        borderRadius: 30,
+    },
+    buttonText: {
+        color: '#fff',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+    signUpButtonText: {
+        color: '#000',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+    paginationContainer: {
+        paddingTop: 8,
+        paddingBottom: 106,
+    },
+    paginationDot: {
+        width: 10,
+        height: 10,
+        borderRadius: 5,
+        marginHorizontal: 8,
+        backgroundColor: 'rgba(0, 0, 0, 0.92)',
+    },
+    inactivePaginationDot: {
+        backgroundColor: 'rgba(0, 0, 0, 0.32)',
+    },
+});
 
 export default OnboardingScreen;
