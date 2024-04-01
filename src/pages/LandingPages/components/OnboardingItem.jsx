56dc116265dc2808764e3e92dc8822bd67fc1c88
idfl0wx <idfl0wxzero@gmail.com>
2024-04-01 19:47:26 +0200

    ^^

--- src/pages/LandingPages/components/OnboardingItem.jsx
+++ src/pages/LandingPages/components/OnboardingItem.jsx
@@ -9,7 +9,7 @@ const OnboardingItem = ({item,isFirst}) => {
     return(
         <View style ={[styles.container, {width}]}>
             <Image source={item.image} style={[styles.images, {width, resizeMode : 'contain',marginBottom: -20, marginTop : 20}]}/>
-            <View style={{flex : 0.65}}>
+            <View style={{flex : 0.50}}>
                 <Text style={TextStyle()}>{item.title}</Text>
                 <Text style = {styles.description}>{item.description}</Text>
             </View>
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
         alignItems : 'center'
     },
     image : {
-        flex : 0.7,
+        flex : 0.6,
         justifyContent : 'center',
     },
     firstTitle: {
@@ -42,15 +42,8 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         color: '#000000',
         marginBottom: '-3%',
+        marginTop: '-10%',
         paddingHorizontal: 20,
-    },
-    title :{
-        fontSize : 32,
-        textAlign: 'center',
-        fontWeight: 'bold',
-        color: '#fff',
-        marginBottom: 5,
-        paddingHorizontal : 50,
     },
     description : {
       fontWeight : '300',
@@ -58,8 +51,9 @@ const styles = StyleSheet.create({
       fontSize: 16,
       padding : 12,
       textAlign: 'center',
+        marginTop: '2%',
       marginBottom: '5%',
-      paddingHorizontal : 25,
+      paddingHorizontal : 10,
     }
 })
 export default OnboardingItem;
